57e4da4fbb186780b6445fcb82b71e2b19e25ef2
Jared <jared@digitalnutt.com>
2020-11-24 17:46:50 -0800

    making things worse and better at the same time

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -1,101 +1,96 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
-import Button from '../button/button';
-import './page.css';
-import mockData from '../../data/mockdata.json';
-import SearchBar from '../searchBar/searchBar';
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import Button from "../button/button";
+import "./page.css";
+import mockData from "../../data/mockdata.json";
+import SearchBar from "../searchBar/searchBar";
+
 // set to false when creating build
+// const LOCAL = process.env.NODE_ENV !== "production" ? true : false;
 const LOCAL = false;
 
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      renderedImages: [],
-      imageNameSearchInput: '',
-      allImages: [],
-      error: null, 
+const Page = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [renderedImages, setRenderedImages] = useState([]);
+  const [imageNameSearchInput, setImageNameSearchInput] = useState("");
+  const [allImages, setAllImages] = useState([]);
+  const [bucketName, setBucketName] = useState("");
+  // const [error, setError] = useState(null);
 
-    }
-    this.getUrlParams = this.getUrlParams.bind(this);
-    this.searchText = this.searchText.bind(this);
-    this.filterImages = this.filterImages.bind(this);
-  }
-
-  getUrlParams() {
+  function getUrlParams() {
     const bucketName = window.location.search;
     return bucketName;
   }
 
-  filterImages() {
-    let filteredImagesData = this.state.allImages.map(image => { return { src: image.src } })
-      .filter(image => image.src.toLowerCase()
-        .includes(this.state.imageNameSearchInput.toLowerCase()));
-    this.setState({
-      renderedImages: filteredImagesData,
-    });
+  function filterImages() {
+    if (imageNameSearchInput.length < 1) {
+      setRenderedImages(allImages);
+    }
+    let filteredImagesData = allImages
+      .map(image => {
+        return { src: image.src };
+      })
+      .filter(image => image.src.toLowerCase().includes(imageNameSearchInput.toLowerCase()));
+    setRenderedImages(filteredImagesData);
   }
 
-  searchText(event) {
-    this.setState({
-      imageNameSearchInput: event.target.value},  
-      () => this.filterImages()
-    )
+  function handleSearch(event) {
+    setImageNameSearchInput(event.currentTarget.value);
+    filterImages();
   }
 
- componentDidMount() {
-   if (!LOCAL) {
-    let bucketName = this.getUrlParams();
-    fetch(`https://5p22w2mvsh.execute-api.us-west-2.amazonaws.com/api/images${bucketName}`)
-    .then(results => {
-      return results.json()
-    })
-    .then(responseImages => {
-      this.setState({renderedImages: responseImages, isLoading: false, allImages: responseImages})
-    })
-   }
-   else {
-     this.setState({ renderedImages: mockData, isLoading: false, allImages: mockData});
-   }
-  }
+  useEffect(() => {
+    // Update the document title using the browser API
+    if (!LOCAL) {
+      let bucketName = getUrlParams();
+      fetch(`https://5p22w2mvsh.execute-api.us-west-2.amazonaws.com/api/images${bucketName}`)
+        .then(results => {
+          return results.json();
+        })
+        .then(responseImages => {
+          debugger;
+          setRenderedImages(responseImages);
+          setLoading(false);
+          setAllImages(responseImages);
+        });
+    } else {
+      setRenderedImages(mockData);
+      setLoading(false);
+      setAllImages(mockData);
+    }
+  }, [isLoading]);
 
-  render() {
-    let bucketName = this.getUrlParams();
-    if (this.state.isLoading === false) {
-        return (
-        <div>
-          <SearchBar searchphrase={this.searchText} expanded={this.state.imageNameSearchInput} />
-          <div className="grid-container">
-            {
-              this.state.renderedImages.map(function (image, index) {
-                const divStyle = {
-                  backgroundImage: 'url(' + image.src + ')'
-                }
+  if (!isLoading) {
+    return (
+      <div>
+        <SearchBar searchphrase={handleSearch} expanded={imageNameSearchInput} />
+        <p>Results: {renderedImages.length}</p>
+        <div className="grid-container">
+          {renderedImages.map(function (image, index) {
+            const divStyle = {
+              backgroundImage: "url(" + image.src + ")"
+            };
 
-                return (
-                  <div className="grid-item" key={index} >
-                    <div className="grid-item__image" style={divStyle} />
-                      <span className="grid-item__image-name">{image.src.split('/').pop()}</span>
-                      <Button imageURL={image.src} bucketName={bucketName} />
-                  </div >
-                )
-
-              })
-            }
-          </div>
+            return (
+              <div className="grid-item" key={index}>
+                <div className="grid-item__image" style={divStyle} />
+                <span className="grid-item__image-name">{image.src.split("/").pop()}</span>
+                <Button imageURL={image.src} bucketName={bucketName} />
+              </div>
+            );
+          })}
         </div>
-        );
-      }
-      return (
-        <div className="loader">
-          <FontAwesomeIcon icon={faCircleNotch} spin/>
-          <div className="loader__text">Loading...</div>
-        </div>
-      )
+      </div>
+    );
+  } else {
+    return (
+      <div className="loader">
+        <FontAwesomeIcon icon={faCircleNotch} spin />
+        <div className="loader__text">Loading...</div>
+      </div>
+    );
   }
-}
-
+};
 
 export default Page;
