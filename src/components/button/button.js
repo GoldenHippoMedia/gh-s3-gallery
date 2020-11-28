8b8d550524d9010c8aa07a91a6a63ee77b0b0c44
Jared <jared@digitalnutt.com>
2020-11-27 17:05:31 -0800

    working on url washer

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,40 +1,35 @@
-import React, { Component } from 'react';
-import './button.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClipboard, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import React, { Component } from "react";
+import "./button.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClipboard, faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 
-class Button extends Component {
-
-  copyURL = () => {
-    console.log("URL copied");
-    const searchParams = new URLSearchParams(this.props.bucketName);
-    const bucketName = searchParams.get('bucket');
-    const formattedUrl = `https://${bucketName}/images/${this.props.imageURL.split('/').pop()}`
+const Button = ({ imageURL, bucketName }) => {
+  function getCDNfromURL(url) {
+    const params = new URLSearchParams(document.location.search).get("region");
+    const region = params.region ? region : "us-west-2";
+    const newURL = url.replace(`s3-${region}.amazonaws.com/`, "");
+    return newURL;
+  }
+  function copyURL() {
+    const formattedUrl = getCDNfromURL(imageURL);
     const dummy = document.createElement("input");
     document.body.appendChild(dummy);
-    dummy.setAttribute('value', formattedUrl);
+    dummy.setAttribute("value", formattedUrl);
     dummy.select();
     document.execCommand("copy");
     document.body.removeChild(dummy);
+    console.log(`URL copied: ${formattedUrl}`);
   }
-
-  openNewWindow = () => {
-    console.log('Open in new window');
-    window.open(this.props.imageURL);
-  }
-
-  render() {
-    return (
-      <div className="actions">
-        <div onClick={this.copyURL} className="actions_button">
-          <FontAwesomeIcon icon={faClipboard} />
-        </div>
-        <div onClick={this.openNewWindow} className="actions_button">
-            <FontAwesomeIcon icon={faExternalLinkAlt} />
-        </div>
+  return (
+    <div className="actions">
+      <div onClick={copyURL} className="actions_button">
+        <FontAwesomeIcon icon={faClipboard} />
       </div>
-    );
-  }
-}
+      <a href={imageURL} className="actions_button" target="_blank">
+        <FontAwesomeIcon icon={faExternalLinkAlt} />
+      </a>
+    </div>
+  );
+};
 
 export default Button;
